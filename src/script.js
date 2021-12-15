34b19afde3d29804ca61a88cb85c74f82cf848cb
tracksuite <youngunner1998@gmail.com>
2021-12-15 10:53:10 +0530

    completed the lesson 25

--- src/script.js
+++ src/script.js
@@ -1,8 +1,15 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import * as dat from 'lil-gui'
-
+/**
+* Loaders
+*/
+const gltfLoader = new GLTFLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+const dracoLoader = new DRACOLoader()
 /**
  * Base
  */
@@ -18,6 +25,86 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 
+
+
+/**
+ * Update all materials
+ */
+ const updateAllMaterials = () =>
+ {
+     scene.traverse((child) =>
+     {
+         if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
+         {
+             child.material.envMap = environmentMap
+             child.material.envMapIntensity = debugObject.envMapIntensity
+             child.castShadow = true
+             child.receiveShadow = true
+             
+         }
+
+     })
+ }
+
+ /**
+ * Environment map
+ */
+  const environmentMap = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.jpg',
+    '/textures/environmentMaps/0/nx.jpg',
+    '/textures/environmentMaps/0/py.jpg',
+    '/textures/environmentMaps/0/ny.jpg',
+    '/textures/environmentMaps/0/pz.jpg',
+    '/textures/environmentMaps/0/nz.jpg'
+])
+
+environmentMap.encoding = THREE.sRGBEncoding
+scene.background = environmentMap
+scene.environment = environmentMap
+debugObject.envMapIntensity = 2.5
+gui.add(debugObject, 'envMapIntensity').min(0).max(10).step(0.001).onChange(updateAllMaterials)
+
+/**
+ * Models
+ */
+
+ dracoLoader.setDecoderPath('/draco/')
+
+ gltfLoader.setDRACOLoader(dracoLoader)
+ 
+ let mixer = null
+ gltfLoader.load(
+    '/models/hamburger2.glb',
+    (gltf) =>
+    {
+        gltf.scene.scale.set(0.5, 0.5, 0.5)
+        gltf.scene.position.set(0, - 1, 0)
+        scene.add(gltf.scene)
+
+        updateAllMaterials()
+    }
+)
+
+
+/**
+ * Lights
+ */
+ const directionalLight = new THREE.DirectionalLight('#ffffff',3)
+ directionalLight.position.set(0.25, 3, - 2.25)
+ directionalLight.castShadow = true
+ directionalLight.shadow.camera.far = 15
+ directionalLight.shadow.mapSize.set(1024, 1024)
+ directionalLight.shadow.normalBias = 0.05
+ scene.add(directionalLight)
+ 
+ const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+ // scene.add(directionalLightCameraHelper)
+ 
+ 
+ gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('lightIntensity')
+ gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001).name('lightX')
+ gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001).name('lightY')
+ gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001).name('lightZ')
 
 /**
  * Sizes
@@ -43,118 +130,18 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
-import { loader } from 'mini-css-extract-plugin'
-
-// ...
-const loader = new DRACOLoader();
-
-loader.setDecoderPath( '/examples/js/libs/draco/' );
-
-loader.preload();
-
-// Load a Draco geometry
-loader.load(
-	// resource URL
-	'model.drc',
-	// called when the resource is loaded
-	function ( geometry ) {
-
-		const material = new THREE.MeshStandardMaterial( { color: 0x606060 } );
-		const mesh = new THREE.Mesh( geometry, material );
-		scene.add( mesh );
-
-	},
-	// called as loading progresses
-	function ( xhr ) {
-
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-	},
-	// called when loading has errors
-	function ( error ) {
-
-		console.log( 'An error happened' );
-
-	}
-);
-
-/**
- * Loaders
- */
-const gltfLoader = new GLTFLoader()
 
 
 
-/**
- * Models
- */
- gltfLoader.load(
-    '/models/hamburger2.glb',
-    (gltf) =>
-    {
-        gltf.scene.scale.set(0.3, 0.3, 0.3)
-        gltf.scene.position.set(0, - 1, 0)
-        scene.add(gltf.scene)
-
-        updateAllMaterials()
-    }
-)
-
-// Texture
-const cubeTextureLoader = new THREE.CubeTextureLoader()
 
 
-/**
- * Lights
- */
-const directionalLight = new THREE.DirectionalLight('#ffffff',3)
-directionalLight.position.set(0.25, 3, - 2.25)
-directionalLight.castShadow = true
-directionalLight.shadow.camera.far = 15
-directionalLight.shadow.mapSize.set(1024, 1024)
-scene.add(directionalLight)
 
-const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
-// scene.add(directionalLightCameraHelper)
 
-/**
- * Update all materials
- */
- const updateAllMaterials = () =>
- {
-     scene.traverse((child) =>
-     {
-         if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
-         {
-             child.material.envMap = environmentMap
-             child.material.envMapIntensity = debugObject.envMapIntensity
-             child.castShadow = true
-             child.receiveShadow = true
-             
-         }
 
-     })
- }
 
-/**
- * Environment map
- */
- const environmentMap = cubeTextureLoader.load([
-    '/textures/environmentMaps/0/px.jpg',
-    '/textures/environmentMaps/0/nx.jpg',
-    '/textures/environmentMaps/0/py.jpg',
-    '/textures/environmentMaps/0/ny.jpg',
-    '/textures/environmentMaps/0/pz.jpg',
-    '/textures/environmentMaps/0/nz.jpg'
-])
-scene.background = environmentMap
-debugObject.envMapIntensity = 2.5
-gui.add(debugObject, 'envMapIntensity').min(0).max(10).step(0.001).onChange(updateAllMaterials)
-environmentMap.encoding = THREE.sRGBEncoding
 
-scene.environment = environmentMap
+
+
 
 /**
  * Camera
@@ -175,19 +162,15 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     antialias: true
 })
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
 renderer.physicallyCorrectLights = true
 renderer.outputEncoding = THREE.sRGBEncoding
 renderer.toneMapping = THREE.ACESFilmicToneMapping
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-//GUIs
-gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('lightIntensity')
-gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001).name('lightX')
-gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001).name('lightY')
-gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001).name('lightZ')
 gui.
     add(renderer, 'toneMapping', {
         No: THREE.NoToneMapping,
